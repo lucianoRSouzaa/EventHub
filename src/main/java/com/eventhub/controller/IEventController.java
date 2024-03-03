c35e515db49fc136472fff153a649f3430ad82db
Luciano Souza <lucianorodriguess101@gmail.com>
2024-03-03 16:41:16 -0300

    feat(EventController): add IEventController interface

---
+++ src/main/java/com/eventhub/controller/IEventController.java
@@ -0,0 +1,11 @@
+package main.java.com.eventhub.controller;
+
+import java.util.List;
+
+import main.java.com.eventhub.model.Event;
+
+public interface IEventController {
+    List<Event> getNextEvents();
+    List<Event> getOccurredEvents();
+    List<Event> getEventsOccurring();
+}
