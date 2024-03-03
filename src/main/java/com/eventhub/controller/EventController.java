786d1a0143f4a242079a468dc58fa8ce61ed9e8e
Luciano Souza <lucianorodriguess101@gmail.com>
2024-03-03 16:40:51 -0300

    refactor(EventController): add check state event

--- src/main/java/com/eventhub/controller/EventController.java
+++ src/main/java/com/eventhub/controller/EventController.java
@@ -4,8 +4,18 @@ import main.java.com.eventhub.model.Event;
 
 import java.util.List;
 
-public class EventController {
-    public List<Event> getEvents() {
-        return Event.readEventsFromFile();
+public class EventController implements IEventController {
+    List<Event> events = Event.readEventsFromFile();
+
+    public List<Event> getNextEvents() {
+        return Event.listUpcomingEvents(events);
+    }
+
+    public List<Event> getOccurredEvents() {
+        return Event.listPastEvents(events);
+    }
+
+    public List<Event> getEventsOccurring() {
+        return Event.listOngoingEvents(events);
     }
 }
