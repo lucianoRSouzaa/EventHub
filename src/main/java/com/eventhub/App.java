bae6a1217f56b7c76836a8d6a5d48c65414c131b
Luciano Souza <lucianorodriguess101@gmail.com>
2024-03-03 11:27:12 -0300

    feat(main): add MainMenu and initial structure

--- src/main/java/com/eventhub/App.java
+++ src/main/java/com/eventhub/App.java
@@ -1,6 +1,36 @@
 package main.java.com.eventhub;
+
+import main.java.com.eventhub.view.MainMenu;
+
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        while (true) {
+            
+            int option = MainMenu.Home();
+            
+            switch (option) {
+                case 1:
+                    // registerUser();
+                    break;
+                case 2:
+                    // createEvent();
+                    break;
+                case 3:
+                    // attendEvent();
+                    break;
+                case 4:
+                    // listUsers();
+                    break;
+                case 5:
+                    // listEvents();
+                    break;
+                case 6:
+                    System.out.println("Saindo...");
+                    return;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
     }
 }
