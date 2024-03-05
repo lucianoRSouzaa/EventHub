b96dd9274c66d7b193e9bc677511789e70fecf12
Luciano Souza <lucianorodriguess101@gmail.com>
2024-03-04 21:48:35 -0300

    feat(UserController): add get method

--- src/main/java/com/eventhub/controller/UserController.java
+++ src/main/java/com/eventhub/controller/UserController.java
@@ -1,5 +1,7 @@
 package main.java.com.eventhub.controller;
 
+import java.util.List;
+
 import main.java.com.eventhub.model.User;
 
 public class UserController {
@@ -7,5 +9,9 @@ public class UserController {
     public static void register(String name, String email, String city, String password){
         User user = new User(name, email, city, password);
         user.saveUser();
+    }
+
+    public static List<User> get(){
+        return User.getAllUsers();
     }
 }
