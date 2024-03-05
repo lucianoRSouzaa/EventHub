c66b84ca3671227731abc99e18b16d74ebeafd55
Luciano Souza <lucianorodriguess101@gmail.com>
2024-03-04 21:02:08 -0300

    feat(EventController): add create method

--- src/main/java/com/eventhub/controller/EventController.java
+++ src/main/java/com/eventhub/controller/EventController.java
@@ -1,7 +1,10 @@
 package main.java.com.eventhub.controller;
 
 import main.java.com.eventhub.model.Event;
+import main.java.com.eventhub.model.EventCategory;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class EventController implements IEventController {
@@ -30,6 +33,18 @@ public class EventController implements IEventController {
 
     public List<Event> getEventByName(String eventName) {
         return Event.getEventByName(events, eventName);
+    }
+
+    public void create(String name, String city, String address, String categoryString, String startDateTimeString, String endDateTimeString, String description){
+        LocalDateTime startDateTime = LocalDateTime.parse(startDateTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        LocalDateTime endDateTime = LocalDateTime.parse(endDateTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+
+        String category = categoryString.toUpperCase();
+
+        EventCategory selectedCategory = EventCategory.valueOf(category);
+
+        Event event = new Event(name, city, address, selectedCategory, startDateTime, endDateTime, description);
+        event.saveEvents();
     }
     
 }
